d3aacf41900c4691ba07f636560cffc5daca7e4b
azgappie <76480024+azgappie@users.noreply.github.com>
2020-12-22 13:53:15 +0100

    v3

--- client.js
+++ client.js
@@ -1,14 +1,18 @@
-// Node.js socket client script
-const net = require('net');
-// Connect to a server @ port 9898
-const client = net.createConnection({ port: 9898 }, () => {
-    console.log('CLIENT: I connected to the server.');
-    client.write('CLIENT: Hello this is client!');
-});
-client.on('data', (data) => {
-    console.log(data.toString());
-    client.end();
-});
-client.on('end', () => {
-    console.log('CLIENT: I disconnected from the server.');
-});
+let socket = new WebSocket("wss://172.20.10.2:9898");
+
+// send message from the form
+document.forms.publish.onsubmit = function () {
+    let outgoingLead = this.lead.value;
+
+    socket.send(outgoingLead);
+    return false;
+};
+
+// message received - show the message in div#messages
+socket.onlead = function (event) {
+    let lead = event.data;
+
+    let leadElem = document.createElement('div');
+    leadElem.textContent = lead;
+    document.getElementById('leads').prepend(leadElem);
+}
--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-// Node.js socket server script
-const net = require('net');
-// Create a server object
-const server = net.createServer((socket) => {
-    socket.on('data', (data) => {
-        console.log(data.toString());
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('lead', function incoming(message) {
+        console.log('received: %s', message);
     });
-    socket.write('SERVER: Hello! This is server speaking.<br>');
-    socket.end('SERVER: Closing connection now.<br>');
-}).on('error', (err) => {
-    console.error(err);
-});
-// Open server on port 9898
-server.listen(9898, () => {
-    console.log('opened server on', server.address().port);
+
+    ws.send('something');
 });
